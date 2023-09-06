510ca5cf0b3e82aa447df4a4d375330273985da0
najah <najahaboobacker79@gmail.com>
2023-09-07 00:53:58 +0530

    ..

---
+++ src/components/trip.js
@@ -0,0 +1,19 @@
+import "./tripstyle.css"
+import TripData from "./tripdata";
+function Trip()
+{
+    return(
+        <div className="trip">
+            <h1>recent trip</h1>
+            <p>explore the world</p>
+    
+        <div className="tripcard">
+           
+            <TripData
+            image1=""/>
+           
+          </div>
+          </div>
+    )
+}
+export default Trip;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import Navbar from '../components/navbar';
 import Hero from '../components/Hero';
 import Destination from '../components/Destination';
+import Trip from '../components/trip';
 
 function Home(){
     return(
@@ -15,6 +16,7 @@ function Home(){
          url="/"
          btnClass="show"/>
          <Destination/>
+         <Trip/>
         </>
     )
 }
